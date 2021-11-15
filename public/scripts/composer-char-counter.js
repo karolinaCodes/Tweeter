acf24632627bfe313ddd92425ef2c3fbeeb8b898
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-15 15:37:12 -0500

    add counter input counter logic

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  $("#tweet-text").on("input", function () {
+    const maxLimit = 140;
+    const inputLength = $(this).val().length;
+    const counterVal = maxLimit - inputLength;
+
+    const counterEl = $(this).parent().find(".counter");
+
+    counterEl.text(counterVal);
+
+    if (counterVal < 0) {
+      counterEl.addClass("invalid");
+    } else {
+      counterEl.removeClass("invalid");
+    }
+  });
+});
