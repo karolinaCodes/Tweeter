08f560ef771970e06ff3a802622bcf7b956b82f0
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-15 22:19:47 -0500

    add ttweet data rendering from server functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(() => {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
-  $("#new-tweet-form").submit(function (e) {
-    e.preventDefault();
-    const tweet = $(this).serialize();
-    console.log(tweet);
-    $.post("/tweets", $(this).serialize());
-  });
-
   // take an array of tweet objects, convert to a tweet elemt, then append each one to the #tweets-container
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
@@ -69,5 +37,18 @@ ${tweetObj.content.text}
     return $tweet;
   };
 
-  renderTweets(data);
+  // on new-tweet-form submission, POST the data to the server
+  $("#new-tweet-form").submit(function (e) {
+    e.preventDefault();
+    const tweet = $(this).serialize();
+    $.post("/tweets", $(this).serialize());
+  });
+
+  const loadTweets = () => {
+    $.get("/tweets", tweets => {
+      renderTweets(tweets);
+    });
+  };
+
+  loadTweets();
 });
