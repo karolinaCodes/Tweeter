294d7ecd3f08c59e5adbd51a3519143538b00ce3
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-16 15:11:09 -0500

    add logic to prevent cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,14 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(() => {
   // take an array of tweet objects, convert to a tweet elemt, then append each one to the #tweets-container
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $("#tweets-container").append($tweet);
+      $("#tweets-container").prepend($tweet);
     }
+  };
+
+  // convert the user input string to a safe "encoded" representation to escape "unsafe" characters (< >)
+  const escape = function (str) {
+    console.log(str);
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   };
 
   // return a tweet <article></article> element with dynamically filled tweet data
@@ -24,7 +31,7 @@ $(document).ready(() => {
   <h4 class="user-handle">${tweetObj.user.handle}</h4>
 </header>
 <div class="tweet-text">
-${tweetObj.content.text}
+${escape(tweetObj.content.text)}
 </div>
 <footer>
   <span class="time-passed">${timeago.format(tweetObj.created_at)}</span>
@@ -37,13 +44,6 @@ ${tweetObj.content.text}
     return $tweet;
   };
 
-  // on new-tweet-form submission, POST the data to the server
-  $("#new-tweet-form").submit(function (e) {
-    e.preventDefault();
-    const tweet = $(this).serialize();
-    $.post("/tweets", $(this).serialize());
-  });
-
   const loadTweets = () => {
     $.get("/tweets", tweets => {
       renderTweets(tweets);
@@ -51,4 +51,33 @@ ${tweetObj.content.text}
   };
 
   loadTweets();
+
+  // on new-tweet-form submission, POST the data to the server
+  $("#new-tweet-form").submit(function (e) {
+    e.preventDefault();
+    // put in constants file?
+    const maxLimit = 140;
+    const $formInputEl = $(this).find("#new-tweet-text");
+    const inputLength = $formInputEl.val().length;
+
+    // if the input is empty, display error message
+    if (!inputLength) {
+      return alert("You did not put any tweet content!");
+    }
+
+    //if tweet is longer than 140 char,  display error message
+    if (maxLimit - inputLength < 0) {
+      return alert("Your tweet content is too long!");
+    }
+
+    const tweet = $(this).serialize();
+    $.post("/tweets", $(this).serialize(), () => {
+      // reset the form and counter
+      $formInputEl.val("");
+      $(this).find(".counter").val(maxLimit);
+
+      // display new tweet
+      loadTweets();
+    });
+  });
 });
