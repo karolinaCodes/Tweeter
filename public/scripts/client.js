531ea6ad6c3fd08a1dde8501dd1897378e1c216a
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-15 22:08:23 -0500

    add form submission functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,36 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(() => {
-  // const data = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac",
-  //     },
-  //     content: {
-  //       text: "If I have seen further it is by standing on the shoulders of giants",
-  //     },
-  //     created_at: 1461116232227,
-  //   },
-  //   {
-  //     user: {
-  //       name: "Descartes",
-  //       avatars: "https://i.imgur.com/nlhLi3I.png",
-  //       handle: "@rd",
-  //     },
-  //     content: {
-  //       text: "Je pense , donc je suis",
-  //     },
-  //     created_at: 1461113959088,
-  //   },
-  // ];
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
 
-  $("#new-tweet-form").submit(e => {
+  $("#new-tweet-form").submit(function (e) {
     e.preventDefault();
+    const tweet = $(this).serialize();
+    console.log(tweet);
+    $.post("/tweets", $(this).serialize());
   });
 
   // take an array of tweet objects, convert to a tweet elemt, then append each one to the #tweets-container
